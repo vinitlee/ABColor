bd000a262b9d5464b65bd677af1c35c22f55f6b0
Vincent Lee <vlee@jawbone.com>
2014-07-14 18:03:12 -0700

    Initial Commit

---
+++ ABC.js
@@ -0,0 +1,8 @@
+abc = function(a,b) {
+  // ARGS: A<a> and B<b> components of LAB value for base color
+  // RTNS: <c>, being a function that takes a value number in an  
+  c = {}
+  // Normal Set
+  // Emphasis 'AXXX' set
+  return c
+}
